13f136a55dde53c9d8f68456d433b29aaf595428
riccardo <id515klx@studenti.univr.it>
2017-09-16 18:35:16 +0200

    Aggiunti models con metodi get e set

--- src/Models/MusicalGenre.java
+++ src/Models/MusicalGenre.java
@@ -6,4 +6,20 @@ package Models;
 public class MusicalGenre {
     private String _id;
     private String _title;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String get_title() {
+        return _title;
+    }
+
+    public void set_title(String _title) {
+        this._title = _title;
+    }
 }
--- src/Models/Sale.java
+++ src/Models/Sale.java
@@ -7,10 +7,44 @@ import java.time.LocalDateTime;
  * Created by riccardo on 16/09/17.
  */
 public class Sale {
+
     public enum PaymentType { Bonifico, CrediCard, Paypal}
     public enum DeliveryType { CourierService, PostalService}
     private Cart _cart;
     private LocalDateTime _saleDate;
     private PaymentType _paymentType;
     private DeliveryType _deliveryType;
+
+    public Cart get_cart() {
+        return _cart;
+    }
+
+    public void set_cart(Cart _cart) {
+        this._cart = _cart;
+    }
+
+    public LocalDateTime get_saleDate() {
+        return _saleDate;
+    }
+
+    public void set_saleDate(LocalDateTime _saleDate) {
+        this._saleDate = _saleDate;
+    }
+
+    public PaymentType get_paymentType() {
+        return _paymentType;
+    }
+
+    public void set_paymentType(PaymentType _paymentType) {
+        this._paymentType = _paymentType;
+    }
+
+    public DeliveryType get_deliveryType() {
+        return _deliveryType;
+    }
+
+    public void set_deliveryType(DeliveryType _deliveryType) {
+        this._deliveryType = _deliveryType;
+    }
+
 }
