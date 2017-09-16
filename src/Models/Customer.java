3c103c544af0665c32a56dd614b62509adc58757
brundev <stefanobrunone.sb@gmail.com>
2017-09-16 17:21:06 +0200

    setter and getters

--- src/Models/Customer.java
+++ src/Models/Customer.java
@@ -5,12 +5,78 @@ package Models;
  */
 public class Customer {
 
-    protected String _CF;
-    protected String _username;
-    protected String _password;
-    protected String _name;
-    protected String _surname;
-    protected String _address;
-    protected String _telephone;
-    protected String _cellphone;
+    private String _CF;
+    private String _username;
+    private String _password;
+    private String _name;
+    private String _surname;
+    private String _address;
+    private String _telephone;
+    private String _cellphone;
+
+    public String get_CF() {
+        return _CF;
+    }
+
+    public void set_CF(String _CF) {
+        this._CF = _CF;
+    }
+
+    public String get_username() {
+        return _username;
+    }
+
+    public void set_username(String _username) {
+        this._username = _username;
+    }
+
+    public String get_password() {
+        return _password;
+    }
+
+    public void set_password(String _password) {
+        this._password = _password;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_surname() {
+        return _surname;
+    }
+
+    public void set_surname(String _surname) {
+        this._surname = _surname;
+    }
+
+    public String get_address() {
+        return _address;
+    }
+
+    public void set_address(String _address) {
+        this._address = _address;
+    }
+
+    public String get_telephone() {
+        return _telephone;
+    }
+
+    public void set_telephone(String _telephone) {
+        this._telephone = _telephone;
+    }
+
+    public String get_cellphone() {
+        return _cellphone;
+    }
+
+    public void set_cellphone(String _cellphone) {
+        this._cellphone = _cellphone;
+    }
+
+
 }
--- src/Models/Product.java
+++ src/Models/Product.java
@@ -10,16 +10,103 @@ import java.util.ArrayList;
  */
 public class Product {
 
-    protected String _code;
-    protected String _title;
-    protected ArrayList<String> _songList;
-    protected BufferedImage _coverImage; //per prelevare immagine da un URL
-    protected float _price;
-    protected LocalDateTime _firstAddedInStore;
-    protected String _artist;
-    protected Musician _description;
-    protected String _genre;
-    protected ArrayList<Musician> _involvedArtists;
-    protected ArrayList<String> _usedInstruments;
+    private String _code;
+    private String _title;
+    private ArrayList<String> _songList;
+    private BufferedImage _coverImage; //per prelevare immagine da un URL
+    private float _price;
+    private LocalDateTime _firstAddedInStore;
+    private String _artist;
+    private Musician _description;
+    private String _genre;
+    private ArrayList<Musician> _involvedArtists;
+    private ArrayList<String> _usedInstruments;
 
+    public String get_code() {
+        return _code;
+    }
+
+    public void set_code(String _code) {
+        this._code = _code;
+    }
+
+    public String get_title() {
+        return _title;
+    }
+
+    public void set_title(String _title) {
+        this._title = _title;
+    }
+
+    public ArrayList<String> get_songList() {
+        return _songList;
+    }
+
+    public void set_songList(ArrayList<String> _songList) {
+        this._songList = _songList;
+    }
+
+    public BufferedImage get_coverImage() {
+        return _coverImage;
+    }
+
+    public void set_coverImage(BufferedImage _coverImage) {
+        this._coverImage = _coverImage;
+    }
+
+    public float get_price() {
+        return _price;
+    }
+
+    public void set_price(float _price) {
+        this._price = _price;
+    }
+
+    public LocalDateTime get_firstAddedInStore() {
+        return _firstAddedInStore;
+    }
+
+    public void set_firstAddedInStore(LocalDateTime _firstAddedInStore) {
+        this._firstAddedInStore = _firstAddedInStore;
+    }
+
+    public String get_artist() {
+        return _artist;
+    }
+
+    public void set_artist(String _artist) {
+        this._artist = _artist;
+    }
+
+    public Musician get_description() {
+        return _description;
+    }
+
+    public void set_description(Musician _description) {
+        this._description = _description;
+    }
+
+    public String get_genre() {
+        return _genre;
+    }
+
+    public void set_genre(String _genre) {
+        this._genre = _genre;
+    }
+
+    public ArrayList<Musician> get_involvedArtists() {
+        return _involvedArtists;
+    }
+
+    public void set_involvedArtists(ArrayList<Musician> _involvedArtists) {
+        this._involvedArtists = _involvedArtists;
+    }
+
+    public ArrayList<String> get_usedInstruments() {
+        return _usedInstruments;
+    }
+
+    public void set_usedInstruments(ArrayList<String> _usedInstruments) {
+        this._usedInstruments = _usedInstruments;
+    }
 }
