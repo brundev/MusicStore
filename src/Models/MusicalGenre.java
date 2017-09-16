40323e49a79363c40eee9382be1a185dfc5ca14e
riccardo <id515klx@studenti.univr.it>
2017-09-16 18:02:02 +0200

    Added model for Customer, Musician and Product

---
+++ src/Models/MusicalGenre.java
@@ -0,0 +1,9 @@
+package Models;
+
+/**
+ * Created by riccardo on 16/09/17.
+ */
+public class MusicalGenre {
+    private String _id;
+    private String _title;
+}
---
+++ src/Models/Song.java
@@ -0,0 +1,14 @@
+package Models;
+
+import java.time.Duration;
+import java.util.Date;
+
+/**
+ * Created by riccardo on 16/09/17.
+ */
+public class Song {
+    private String _id;
+    private String _title;
+    private Duration _duration;
+    private Date _releaseDate;
+}
