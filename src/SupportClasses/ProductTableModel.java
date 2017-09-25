a6dc9affb4532ab4ed98814b334de16c9bd7bed6
riccardo_gobbi <riccardo.gobbi.95@gmail.com>
2017-09-26 00:54:41 +0200

    fix vari

--- src/SupportClasses/ProductTableModel.java
+++ src/SupportClasses/ProductTableModel.java
@@ -9,7 +9,7 @@ public class ProductTableModel extends DefaultTableModel{
 
     public ProductTableModel()
     {
-        super(null, new String[]{"Titolo", "Immagine", "Prezzo", "Descrizione", "Autore", "Genere"});
+        super(null, new String[]{"",""});
     }
 
     @Override
