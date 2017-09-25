b424f7580c6f248ee047f58ea17d120d3400bfd1
brundev <stefanobrunone.sb@gmail.com>
2017-09-25 17:53:56 +0200

    mod

--- src/Models/Product.java
+++ src/Models/Product.java
@@ -21,7 +21,6 @@ public class Product extends Subject{
     private LocalDateTime _firstAddedInStore;
     private String _description;
     private Musician _artist;
-    private String _artistName;
     private String _genre;
     private ArrayList<Musician> _involvedArtists;
     private ArrayList<String> _usedInstruments;
@@ -166,11 +165,4 @@ public class Product extends Subject{
         return _productStocks;
     }
 
-    public String get_artistName() {
-        return _artistName;
-    }
-
-    public void set_artistName(String _artistName) {
-        this._artistName = _artistName;
-    }
 }
