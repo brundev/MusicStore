59a4e4d2902a2b2170c5fbb7a3ae534110e325b4
brundev <stefanobrunone.sb@gmail.com>
2017-09-25 03:39:10 +0200

    finished cartcontroller

--- src/Controllers/CartController.java
+++ src/Controllers/CartController.java
@@ -63,8 +63,6 @@ public class CartController {
             p.set_price(rs.getFloat(2));
             p.set_coverImage(rs.getString(3));
             _cart.addToCart(p);
-
-            //addToCart(p);
         }
 
 
@@ -95,7 +93,7 @@ public class CartController {
 
             c[b.length]=p.get_code();
 
-            query = "UPDATE sale SET products = "+conn.createArrayOf(a.getBaseTypeName(),c)+" where sale.username ILIKE ?;";
+            query = "UPDATE sale SET products = '"+conn.createArrayOf(a.getBaseTypeName(),c)+"' where sale.username ILIKE ?;";
             stmt = conn.prepareStatement(query);
             stmt.setString(1,cartUser);
             stmt.executeUpdate();
@@ -110,6 +108,47 @@ public class CartController {
 
 
     //TODO
-    //public void removeFromCart(Product p)
-    //
+    public void removeFromCart(Product p){
+
+        String cartUser = _cart.get_user().get_username();
+
+        try {
+            Connection conn = DBConnSingleton.getConn();
+            String query = "select products from sale where sale.username ILIKE ?;";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1,cartUser);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+
+            Array a = rs.getArray(1);
+            Integer b []= (Integer[])a.getArray();
+            Integer c[] = new Integer[b.length-1];
+
+            int ct=0;
+            boolean removed=false;
+
+            for(int i=0;i<b.length;i++){
+                if(p.get_code()!=b[i] || removed) {
+                    c[ct] = b[i];
+                    ct++;
+                }
+                else
+                {
+                    removed=true;
+                }
+            }
+
+            query = "UPDATE sale SET products = '"+conn.createArrayOf(a.getBaseTypeName(),c)+"' where sale.username ILIKE ?;";
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1,cartUser);
+            stmt.executeUpdate();
+
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
