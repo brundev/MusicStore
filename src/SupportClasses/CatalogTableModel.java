48972770757e99ca7b22081d615ccd53f4278385
brundev <stefanobrunone.sb@gmail.com>
2017-09-25 22:36:14 +0200

    factory

---
+++ src/SupportClasses/CatalogTableModel.java
@@ -0,0 +1,22 @@
+package SupportClasses;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/**
+ * Created by stef on 25/09/2017.
+ */
+public class CatalogTableModel extends DefaultTableModel {
+
+
+    private String[] _columnNames = {"Titolo", "Immagine", "Prezzo", "Descrizione", "Autore", "Genere"};
+
+    public CatalogTableModel() {
+        DefaultTableModel a = new DefaultTableModel(null, _columnNames) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }};
+    }
+
+}
